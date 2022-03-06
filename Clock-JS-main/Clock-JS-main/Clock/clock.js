12c7b7ed98dc55e14bb075f8423a6d097cc7d82f
sourab19198 <51846406+sourab19198@users.noreply.github.com>
2022-03-06 23:53:24 +0530

    Add files via upload

---
+++ Clock-JS-main/Clock-JS-main/Clock/clock.js
@@ -0,0 +1,20 @@
+
+setInterval(() => {
+	d = new Date(); //object of date()
+	hr = d.getHours();
+	min = d.getMinutes();
+	sec = d.getSeconds();
+	hr_rotation = 30 * hr + min / 2; //converting current time
+	min_rotation = 6 * min;
+	sec_rotation = 6 * sec;
+
+    document.getElementById("a1").innerHTML = hr ;
+    document.getElementById("a2").innerHTML = min ;
+    document.getElementById("a3").innerHTML = sec ;
+
+
+	hour.style.transform = `rotate(${hr_rotation}deg)`;
+	minute.style.transform = `rotate(${min_rotation}deg)`;
+	second.style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000);
+
